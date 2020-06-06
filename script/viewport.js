ec7b82415cae27a519883f8b416f627cf0cac22e
lfz084 <163084wb@163.com>
2020-06-06 14:47:04 +0800

    viewport

--- script/viewport.js
+++ script/viewport.js
@@ -3,20 +3,19 @@ function view() {
   this.viewport.setAttribute("name", "viewport");
   this.viewport.setAttribute("content", "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0");
   document.head.appendChild(this.viewport);
-  this.scale=1;
-  /*
   let dw=document.documentElement.clientWidth;//device-width
   this. scale = dw/1000;
   this.viewport.setAttribute("content", "width=1000, initial-scale=" +this.scale+", maximum-scale=1.0, user-scalable=0");
-*/
+  alert(this.scale);
+  
 }
 
 view.prototype.resize = function() {
   this.viewport.setAttribute("content", "initial-scale=" + 0.1);
-  this.viewport.setAttribute("content", "width=device-width, initial-scale=" +this.scale+",  user-scalable=0");
+  this.viewport.setAttribute("content", "width=1000, initial-scale=" +this.scale+",  user-scalable=0");
 };
 
 view.prototype.userScalable = function(scalable) {
   this.viewport.setAttribute("content", "initial-scale=" + 0.1);
-  this.viewport.setAttribute("content", "width=device-width, initial-scale=" +this.scale+",  user-scalable=1");
+  this.viewport.setAttribute("content", "width=1000, initial-scale=" +this.scale+",  user-scalable=1");
 };
