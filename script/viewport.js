1be34b50ab4ebe77b63fb72757606b97d560dc89
lfz084 <163084wb@163.com>
2020-06-06 15:23:13 +0800

    view

--- script/viewport.js
+++ script/viewport.js
@@ -1,23 +1,23 @@
 function view() {
   this.viewport = document.createElement("meta");
   this.viewport.setAttribute("name", "viewport");
-  this.viewport.setAttribute("content", "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0");
+  this.viewport.setAttribute("content", "width=device-width, initial-scale=1.0, user-scalable=no");
   document.head.appendChild(this.viewport);
   let dw=document.documentElement.clientWidth;//device-width
   this. scale = dw/1000;
-  this.viewport.setAttribute("content", "width=1000, initial-scale=" +this.scale+", maximum-scale=1.0, user-scalable=0");
+  this.viewport.setAttribute("content", "width=1000, initial-scale=" +this.scale+", user-scalable=no");
   alert(this.scale);
   
 }
 
 view.prototype.resize = function() {
   this.viewport.setAttribute("content", "initial-scale=" + 0.1);
-  this.viewport.setAttribute("content", "width=1000, initial-scale=" +this.scale+",  user-scalable=0");
+  this.viewport.setAttribute("content", "width=1000, initial-scale=" +this.scale+",  user-scalable=no");
   alert(this.viewport.getAttribute("content")) ;
 };
 
 view.prototype.userScalable = function(scalable) {
   this.viewport.setAttribute("content", "initial-scale=" + 0.1);
-  this.viewport.setAttribute("content", "width=1000, initial-scale=" +this.scale+",  user-scalable=1");
+  this.viewport.setAttribute("content", "width=1000, initial-scale=" +this.scale+",  user-scalable=yes");
   alert(this.viewport.getAttribute("content"));
 };
