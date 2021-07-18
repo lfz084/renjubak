617d317e7132ba7b418f3b7234b66d170786ddfe
lfz084 <163084wb@163.com>
2021-07-19 02:37:49 +0800

    loading svg

--- sw.js
+++ sw.js
@@ -1,7 +1,52 @@
-var VERSION = 'v0715';
+var VERSION = 'v0718';
+
+let load = (() => {
+    let urls = [];
+    let timer = null;
+
+    function pushURL(url) {
+        if (urls.indexOf(url) < 0) {
+            urls.push(url);
+        }
+    }
+
+    function removeURL(url) {
+        let idx = urls.indexOf(url);
+        if (idx + 1) {
+            urls.splice(idx, 1);
+        }
+    }
+
+    function interval() {
+        
+        if (urls.length == 0) {
+            clearInterval(timer);
+            timer = null;
+            postMsg(`load finish`);
+        }
+    }
+    
+    return {
+        loading: (msg) => {
+            let url = msg;
+            if (url.indexOf("worker.js")+1) return;
+            if (!timer) {
+                timer = setInterval(interval, 1000);
+                postMsg(`loading......`);
+            }
+            pushURL(url);
+
+        },
+        finish: (msg) => {
+            let url = msg;
+            removeURL(url);
+        }
+    };
+})();
 
 // 缓存
 self.addEventListener('install', function(event) {
+    postMsg(`service worker install...`);
     event.waitUntil(
         caches.open(VERSION).then(function(cache) {
             return cache.addAll([
@@ -14,6 +59,7 @@ self.addEventListener('install', function(event) {
 
 // 缓存更新
 self.addEventListener('activate', function(event) {
+    postMsg(`service worker activate...`);
     event.waitUntil(
         caches.keys().then(function(cacheNames) {
             return Promise.all(
@@ -30,26 +76,37 @@ self.addEventListener('activate', function(event) {
 
 // 捕获请求并返回缓存数据
 self.addEventListener('fetch', function(event) {
+    
+    const _URL = event.request.url;
+    load.loading(_URL);
+    //postMsg(`请求资源\n url=${_URL}`);
     event.respondWith(
         caches.match(event.request).then(response => {
             if (response.ok) {
-                getFetch()
+                load.finish(_URL);
+                //postMsg(`读取缓存成功\n url=${_URL}`);
+                getFetch();
                 return response;
             }
             else {
+                //postMsg(`从网络下载资源\n url=${_URL}`);
                 return getFetch();
             }
         })
         .catch(err => {
+            //postMsg(`下载失败，重试一次\n url=${_URL}`);
             return getFetch();
         })
     )
 
     function getFetch() {
+
         return new Promise((resolve, reject) => {
             fetch(event.request)
                 .then(response => {
                     if (response.ok) {
+                        load.finish(_URL);
+                        //postMsg(`下载资源完成\n url=${_URL}`);
                         let cloneRes = response.clone();
                         caches.open(VERSION).then(cache => {
                             cache.put(event.request, cloneRes);
@@ -60,10 +117,17 @@ self.addEventListener('fetch', function(event) {
                         reject();
                     }
                 })
-                .catch(err=>{
+                .catch(err => {
                     reject();
                 })
         })
     }
-
 });
+
+function postMsg(msg) {
+    return self.clients.matchAll().then(function(clients) {
+        return Promise.all(clients.map(function(client) {
+            return client.postMessage(msg);
+        }));
+    });
+}
